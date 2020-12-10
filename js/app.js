a47e91c9725f08f3c532f7a5a5c0335b7dfba7e2
kamal1997-dev <2016kuec2041@iiitkota.ac.in>
2020-12-10 16:22:17 +0530

    my first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+let hamberger=document.querySelector('.hamberger');
+let times=document.querySelector('.times');
+let mobnav = document.querySelector('.mob-nav');
+hamberger.addEventListener('click',function(){
+    mobnav.classList.add('open');
+
+
+});
+times.addEventListener('click',function(){
+    mobnav.classList.remove('open');
+    
+
+});
+
+
+});
