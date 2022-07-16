0ae94915acd7d762b7d91152ffeb7e3dbb73fa65
kamal1997-dev <75772527+kamal1997-dev@users.noreply.github.com>
2022-07-16 17:47:07 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-let hamberger=document.querySelector('.hamberger');
-let times=document.querySelector('.times');
-let mobnav = document.querySelector('.mob-nav');
-hamberger.addEventListener('click',function(){
-    mobnav.classList.add('open');
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobnav = document.querySelector('.mob-nav');
+    hamberger.addEventListener('click', function () {
+        mobnav.classList.add('open');
 
 
-});
-times.addEventListener('click',function(){
-    mobnav.classList.remove('open');
-    
+    });
+    times.addEventListener('click', function () {
+        mobnav.classList.remove('open');
 
-});
+
+    });
 
 
 });
